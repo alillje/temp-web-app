3b3ae83cbe3fa5ba9ed37594dc2ff11955eb2a0f
Andreas Lillje <al225wg@student.lnu.se>
2023-05-03 13:32:19 +0200

    Add nav menu component

---
+++ src/components/nav-menu/nav-menu.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import { getLatestTempdata } from '../../services/fetch-service'
+import dayjs from 'dayjs'
+
+/**
+ * NavMenu Component.
+ */
+const NavMenu = () => {
+  return (
+<div className="nav-menu-container"></div>
+  )
+}
+
+export default NavMenu
