3f2702f0f14704f00ab56f64a06b83dbebd1dfc8
Andreas Lillje <al225wg@student.lnu.se>
2023-05-04 13:50:59 +0200

    Fix timezone diff

--- src/components/average-item/average-item.js
+++ src/components/average-item/average-item.js
@@ -1,18 +1,12 @@
-import utc from 'dayjs-plugin-utc'
-import timezone from 'dayjs-plugin-timezone'
 import dayjs from 'dayjs'
 import './average-item.css'
-
-// Extend dayjs with plugins
-dayjs.extend(utc)
-dayjs.extend(timezone)
 
 /**
  * AverageItem Component.
  */
 const AverageItem = ({ temperature = 23.3, datetime = '2023-05-02T19:00:00Z', hour = true }) => {
-  const time = dayjs(datetime).tz('Europe/Stockholm')
-  console.log(datetime)
+  // Subtract 2 hours to account for timzeone difference
+  const time = dayjs(datetime).subtract(2, 'hour')
   return (
     <div className="hour-average-item">
       <div className="itemTime">{hour ? time.format('HH:mm') : time.format('dddd')}</div>
